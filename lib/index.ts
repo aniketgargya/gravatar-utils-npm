8424c25610f47f803ad1268b6db41aee84f8d873
Aniket Gargya <gargya.aniket@gmail.com>
2020-07-27 16:04:30 -0500

    added gravatar avatar function

--- lib/index.ts
+++ lib/index.ts
@@ -3,3 +3,5 @@ import { createHash } from "crypto";
 export const getGravatarHash = (email: string): string => createHash("md5").update(email).digest("hex");
 
 export const getGravatarUrl = (email: string): string => `https://www.gravatar.com/${getGravatarHash(email)}.json`;
+
+export const getGravatarAvatar = (email: string): string => `https://secure.gravatar.com/avatar/${getGravatarHash(email)}`;
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-const { getGravatarHash, getGravatarUrl } = require("./../dist/");
+const { getGravatarHash, getGravatarUrl, getGravatarAvatar } = require("./../dist/");
 
 test("Checking Get Gravatar Hash", () => {
     expect(getGravatarHash("gargya.aniket@gmail.com")).toBe("15d4fc7c2997c7869b3a855fa01cb6d4");
@@ -6,4 +6,8 @@ test("Checking Get Gravatar Hash", () => {
 
 test("Checking Get Gravatar URL", () => {
     expect(getGravatarUrl("gargya.aniket@gmail.com")).toBe("https://www.gravatar.com/15d4fc7c2997c7869b3a855fa01cb6d4.json");
+});
+
+test("Checking Get Gravatar Avatar", () => {
+    expect(getGravatarAvatar("gargya.aniket@gmail.com")).toBe("https://secure.gravatar.com/avatar/15d4fc7c2997c7869b3a855fa01cb6d4");
 });
